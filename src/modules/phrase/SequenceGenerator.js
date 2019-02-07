444186c90f72332f4e3fed19eba21f3b48e81ca0
davidr <davidrowthorn@gmail.com>
2019-02-07 20:50:00 +0000

    Sequence generation test passes

--- src/modules/phrase/SequenceGenerator.js
+++ src/modules/phrase/SequenceGenerator.js
@@ -6,10 +6,28 @@ export default class SequenceGenerator {
     }
 
     generate = () => {
+        let lastNote = 'z'
+        let consecutive = 1
         let result = ''
-        let l = this.length
-        while (l--) {
-            result += this.noteGenerator.generate()
+        let remaining = this.length
+
+        while (remaining--) {
+            let note = this.noteGenerator.generate()
+
+            if (note === lastNote && ++consecutive > this.maxConsecutive) {
+                let attempts = 0
+                while (note === lastNote) {
+                    note = this.noteGenerator.generate()
+                    if (++attempts > 8) {
+                        note = 'z'
+                        attempts = 0
+                    }
+                }
+                consecutive = 1
+            }
+
+            result += note
+            lastNote = note
         }
 
         return result
--- src/modules/phrase/SequenceGenerator.test.js
+++ src/modules/phrase/SequenceGenerator.test.js
@@ -1,11 +1,10 @@
 import SequenceGenerator from './SequenceGenerator'
 
 test('Returns a sequence containing no more than the specified maximum consecutive notes', () => {
-    const MockNoteGenerator = {}
-    let counter = 0;
+    const MockNoteGenerator = {
+        generate: () => 'c'
+    }
 
-    MockNoteGenerator.generate = () => 'c'
-
-    const sg = new SequenceGenerator({length: 8, noteGenerator: MockNoteGenerator})
-    expect(sg.generate()).toBe('ccczcccz')
+    const sg = new SequenceGenerator({length: 16, noteGenerator: MockNoteGenerator})
+    expect(sg.generate()).toBe('ccczccczccczcccz')
 })

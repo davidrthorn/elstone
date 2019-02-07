cc815d5352756a58c152fb50632f067b3fef850b
davidr <davidrowthorn@gmail.com>
2019-02-07 19:39:09 +0000

    sequence generator test failing

--- src/modules/phrase/SequenceGenerator.js
+++ src/modules/phrase/SequenceGenerator.js
@@ -1,7 +1,17 @@
-export class SequenceGenerator {
+export default class SequenceGenerator {
     constructor ({length=16, maxConsecutive=3, noteGenerator}) {
         this.length = length
         this.maxConsecutive = maxConsecutive
         this.noteGenerator = noteGenerator
+    }
+
+    generate = () => {
+        let result = ''
+        let l = this.length
+        while (l--) {
+            result += this.noteGenerator.generate()
+        }
+
+        return result
     }
 }
--- src/modules/phrase/SequenceGenerator.test.js
+++ src/modules/phrase/SequenceGenerator.test.js
@@ -1,9 +1,11 @@
 import SequenceGenerator from './SequenceGenerator'
 
 test('Returns a sequence containing no more than the specified maximum consecutive notes', () => {
+    const MockNoteGenerator = {}
     let counter = 0;
-    const MockGenerator = () =>
-        counter++ % 4 === 0
-            ? 'z'
-            : 'c'
+
+    MockNoteGenerator.generate = () => 'c'
+
+    const sg = new SequenceGenerator({length: 8, noteGenerator: MockNoteGenerator})
+    expect(sg.generate()).toBe('ccczcccz')
 })
